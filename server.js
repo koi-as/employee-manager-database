669d658fc6fed56567746f8505c4a10ea9187f1d
Koi-AS <lightningblade.901@gmail.com>
2023-05-05 11:45:02 -0400

    Installed express.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const express = require('express');
 
 const PORT = process.env.PORT || 9362;
 const app = express();
