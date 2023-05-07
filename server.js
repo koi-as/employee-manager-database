b52c41a8e2c98311f7331dff70521d0347921596
Koi-AS <lightningblade.901@gmail.com>
2023-05-07 12:53:08 -0400

    Switched inquirer versions and set up some logic for the handling of the database program

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express');
 
-const PORT = process.env.PORT || 9362;
 const app = express();
 
 const { 
@@ -21,12 +20,67 @@ const db = mysql.createConnection(
     }
 );
 
-inquirer
-    .prompt(menuQuestion)
-    .then((responses) => {
-        console.log(responses)
+const init = () => {
+    inquirer
+        .prompt(menuQuestion)
+        .then((data) => {
+            if (data.menu = 'View all departments') {
+                db.query('SELECT * FROM department', (err, results) => {
+                    console.log(results);
+                });
+                init();
+            } else if (data.menu = 'View all roles') {
+                db.query('SELECT * FROM role', (err, results) => {
+                    console.log(results);
+                });
+                init();
+            } else if (data.menu = 'View all employees') {
+                db.query('SELECT * FROM employee', (err, results) => {
+                    console.log(results);
+                });
+                init();
+            } else if (data.menu = 'Add a department') {
+                inquirer
+                    .prompt(addDepartmentQuestion)
+                    .then((newDepData) => {
+                        console.log(newDepData);
+                    });
+                    db.query('SELECT * FROM department', (err, results) => {
+                        console.log(results);
+                    });
+                init();
+            } else if (data.menu = 'Add a role') {
+                inquirer
+                    .prompt(addRoleQuestion)
+                    .then((newRoleData) => {
+                        console.log(newRoleData);
+                    });
+                db.query('SELECT * FROM role', (err, results) => {
+                    console.log(results);
+                });
+                init();
+            } else if (data.menu = 'Add an employee') {
+                inquirer
+                    .prompt(addEmployeeQuestion)
+                    .then((newEmplData) => {
+                        console.log(newEmplData);
+                    });
+                db.query('SELECT * FROM employee', (err, results) => {
+                    console.log(results);
+                });
+                init();
+            } else if (data.menu = 'Update an employee role') {
+                inquirer
+                    .prompt(updateEmployeeQuestion)
+                    .then((updateEmplRole) => {
+                        console.log(updateEmplRole);
+                    });
+                db.query('SELECT * FROM employee', (err, results) => {
+                    console.log(results);
+                });
+                init();
+            }
     });
+}
 
-app.listen(PORT, () => {
-    console.log(`App running at http://localhost:${PORT}`)
-})
+init()
