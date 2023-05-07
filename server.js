ca75d54b0aa8b4ba74fde9ee09e92424be9b7ed4
Koi-AS <lightningblade.901@gmail.com>
2023-05-06 23:53:43 -0400

    Added questions for the functionality

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express');
@@ -6,14 +5,27 @@ const express = require('express');
 const PORT = process.env.PORT || 9362;
 const app = express();
 
+const { 
+    menuQuestion, 
+    addDepartmentQuestion, 
+    addRoleQuestion, 
+    addEmployeeQuestion, 
+    updateEmployeeQuestion } = require('./lib/questions.js');
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: process.env.USER,
         password: process.env.PASSWORD,
-        database: ''
+        database: 'employee_db'
     }
-)
+);
+
+inquirer
+    .prompt(menuQuestion)
+    .then((responses) => {
+        console.log(responses)
+    });
 
 app.listen(PORT, () => {
     console.log(`App running at http://localhost:${PORT}`)
